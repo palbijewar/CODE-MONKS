749fb0c27afe2ee4e26b7c767a547eafdc519c5b
Pal Bijewar <pal.bijewar@gmail.com>
2023-07-24 17:25:05 +0530

    final commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,15 +4,15 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="/home">
+        <a href="#home">
           <h1>Code Monks</h1>
         </a>
       </div>
 
       <div className="nav-items">
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
       </div>
     </nav>
   );
