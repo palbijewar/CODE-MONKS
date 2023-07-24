34f88198a19969c1f1661c974f2c9a372b6f83ae
Pal Bijewar <pal.bijewar@gmail.com>
2023-07-24 15:58:38 +0530

    home page created

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="home" id='home'>
+      <div className="intro">
+        <h1 className='title'>Hello Programmers</h1>
+        <p className='pera'>Welcome to the mesmerizing world of Web Development Classes! Dive into the realm of boundless creativity and cutting-edge technologies. Unleash your coding prowess through our expert-led courses, from fundamental building blocks to dazzling masterpieces. Join our dynamic community, where innovation thrives, and together, let's weave the web of your dreams and shape a digital tomorrow that astounds the world!</p>
+      </div>
+      <div className="hero">
+        <img src="/images/hero.png" alt="hero" />
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/home">
+          <h1>Code Monks</h1>
+        </a>
+      </div>
+
+      <div className="nav-items">
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
